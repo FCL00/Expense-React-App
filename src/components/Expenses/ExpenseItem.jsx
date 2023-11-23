b18122372936b48946cf8bfb7e1fb40083373d2d
FCL00 <flagahit00@gmail.com>
2023-11-23 20:29:58 +0800

    one state and multiple state

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./ExpenseItem.css";
@@ -5,20 +6,13 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  const ClickHandler = () => {
-    setTitle("Updated");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={ClickHandler}>Click me</button>
     </Card>
   );
 }
--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -3,20 +3,57 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setEnteredTitle(value);
+  };
+
+  const amountChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setEnteredAmount(value);
+  };
+
+  const dateChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setEnteredDate(value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input
+            name="title"
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            name="amount"
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+            value={enteredAmount}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-01-01" max="2024-12-01" />
+          <input
+            name="date"
+            type="date"
+            min="2022-01-01"
+            max="2024-12-01"
+            onChange={dateChangeHandler}
+            value={enteredDate}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
@@ -27,3 +64,21 @@ function ExpenseForm() {
 }
 
 export default ExpenseForm;
+
+// one state approach
+// const [InputValues, setInputValues] = useState({
+//   title: "",
+//   amount: "",
+//   date: "",
+// });
+
+// const inputChangeHandler = (event) => {
+//   const { name, value } = event.target;
+//   setInputValues((preValue) => {
+//     return {
+//       ...preValue,
+//       [name]: value,
+//     };
+//   });
+//   console.log(InputValues);
+// };
